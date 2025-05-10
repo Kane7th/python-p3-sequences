2bddb632226471d72228034b4974d42d588ee340
Kane Kabena <onekaneldn@gmail.com>
2025-05-10 14:28:23 +0300

    python sequences lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # No numbers to show
+    elif length == 1:
+        print([0])  # Only the first number
+    elif length == 2:
+        print([0, 1])  # First two numbers
+    else:
+        fib = [0, 1]
+        for i in range(2, length):
+            next_num = fib[-1] + fib[-2]
+            fib.append(next_num)
+        print(fib)
